ac34229f7bd9feccd2aff9d145a37c556d896090
Nidhul <41993322+Nidhul@users.noreply.github.com>
2024-07-30 16:32:35 +0530

    Update settings.py

--- Gstock/settings.py
+++ Gstock/settings.py
@@ -14,7 +14,10 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = '/static/'
 
+# Directory where collectstatic will store collected files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
